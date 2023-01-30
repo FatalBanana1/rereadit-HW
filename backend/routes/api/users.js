837503ee801063bb37c3fd3300eb8514929ec78a
Henry W <henry.woodmansee@gmail.com>
2023-01-30 12:07:34 -0800

    imported validate signup from validation chains

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,46 +6,13 @@ const express = require("express");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const { check } = require("express-validator");
-const { handleValidationErrors } = require("../../utils/validation");
+const {
+	handleValidationErrors,
+	validateSignup
+} = require("../../utils/validation");
 const router = express.Router();
 
 // routes
-
-/*
-//testing post route
-fetch('/api/users', {
-  method: 'POST',
-  headers: {
-    "Content-Type": "application/json",
-    "XSRF-TOKEN": `sxvI7aLo-k4ORa8dzJYGQ1lqwGpxcMFSnSS8`
-  },
-  body: JSON.stringify({
-    email: 'spidey@spider.man',
-    username: 'Spidey',
-    password: 'password'
-  })
-}).then(res => res.json()).then(data => console.log(data));
-*/
-
-const validateSignup = [
-	check("email")
-		.exists({ checkFalsy: true })
-		.isEmail()
-		.withMessage("Please provide a valid email."),
-	check("username")
-		.exists({ checkFalsy: true })
-		.isLength({ min: 4 })
-		.withMessage("Please provide a username with at least 4 characters."),
-	check("username")
-		.not()
-		.isEmail()
-		.withMessage("Username cannot be an email."),
-	check("password")
-		.exists({ checkFalsy: true })
-		.isLength({ min: 6 })
-		.withMessage("Password must be 6 characters or more."),
-	handleValidationErrors,
-];
 
 // Sign up
 router.post("/", validateSignup, async (req, res, next) => {
@@ -57,16 +24,16 @@ router.post("/", validateSignup, async (req, res, next) => {
 			firstName,
 			lastName,
 			email,
-			username,
-		},
+			username
+		}
 	});
 	if (check) {
 		return res.status(403).json({
 			message: "User already exists",
 			statusCode: 403,
 			errors: {
-				email: "User with that email already exists",
-			},
+				email: "User with that email already exists"
+			}
 		});
 	}
 
@@ -76,14 +43,14 @@ router.post("/", validateSignup, async (req, res, next) => {
 		lastName,
 		email,
 		username,
-		password,
+		password
 	});
 
 	await setTokenCookie(res, user);
 
 	user = await User.scope(["defaultScope"]).findOne({
 		attributes: ["id", "firstName", "lastName", "email"],
-		where: { firstName, lastName, email, username },
+		where: { firstName, lastName, email, username }
 	});
 
 	return res.json({
@@ -92,8 +59,8 @@ router.post("/", validateSignup, async (req, res, next) => {
 			firstName,
 			lastName,
 			email,
-			username,
-		},
+			username
+		}
 	});
 });
 
