48e31f88f3a4d6f1336f780262b13373e940bd1f
Henry W <henry.woodmansee@gmail.com>
2023-01-31 12:10:50 -0800

    idk

---
+++ frontend/src/components/Subreadit/SubreaditIndex/index.js
@@ -0,0 +1,59 @@
+// subreadit index
+
+//imports
+import React, { useEffect, useState } from "react";
+import * as sessionActions from "../../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useHistory } from "react-router-dom";
+import { thunkReadSubreadits } from "../../../store/subreadits";
+import ReadSubreadit from "../ReadSubreadit";
+
+//main
+const SubreaditIndex = () => {
+	let dispatch = useDispatch();
+	let [isLoaded, setIsLoaded] = useState(false);
+
+	useEffect(() => {
+		dispatch(thunkReadSubreadits())
+			.then(() => setIsLoaded(true))
+			.catch((e) => e.errors);
+	}, [dispatch]);
+
+	let subs = useSelector((state) => state.subreadits);
+	let subreadits = Object.values(subs);
+
+	//return
+	return (
+		<div>
+			<div>Connected to Subreadit Index Component!!!</div>
+			{isLoaded && (
+				<div className="subs-container">
+					{subreadits.map((sub) => (
+						<NavLink key={sub.id} to={`/sub/${sub.id}`}>
+							<ReadSubreadit sub={sub} />
+						</NavLink>
+					))}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default SubreaditIndex;
+
+/*
+
+{
+Admin {id, username}
+SubscriberCount,
+about,
+adminId,
+bannerImage,
+category,
+circleImage,
+id,
+name,
+}
+
+
+*/
---
+++ frontend/src/store/subreadits.js
@@ -0,0 +1,188 @@
+//subreadits reducer
+
+//imports
+import { csrfFetch } from "./csrf";
+
+//----------------------------------------------
+
+//types crud - subreadits
+const READ_SUBREADITS = `subreadits/READ`;
+const READ_SUBREADIT_DETAILS = `subreadit/READ_DETAILS`;
+const CREATE_SUBREADIT = `subreadits/CREATE`;
+const UPDATE_SUBREADIT = `subreadits/UPDATE`;
+const DELETE_SUBREADIT = `subreadits/DELETE`;
+const RESET_SUBREADIT = "subreadits/resetState";
+
+//----------------------------------------------
+
+//regular actions
+//read
+const actionReadSubreadits = (subreadits) => ({
+	type: READ_SUBREADITS,
+	subreadits,
+});
+
+//read group details
+const actionReadSubreaditDetails = (subreadit) => ({
+	type: READ_SUBREADIT_DETAILS,
+	subreadit,
+});
+
+//create
+const actionCreateSubreadit = (subreadit) => ({
+	type: CREATE_SUBREADIT,
+	subreadit,
+});
+
+//update
+const actionUpdateSubreadit = (subreadit) => ({
+	type: UPDATE_SUBREADIT,
+	subreadit,
+});
+
+//delete
+const actionDeleteSubreadit = (subreadit) => ({
+	type: DELETE_SUBREADIT,
+	subreadit,
+});
+
+//reset
+export const actionResetSubreadits = () => ({
+	type: RESET_SUBREADIT,
+});
+
+//----------------------------------------------
+
+//thunk actions
+
+// GET: Get All Subreadits Route: /api/sub
+export const thunkReadSubreadits = () => async (dispatch) => {
+	let response = await csrfFetch(`/api/sub`);
+
+	// console.log(`thunk>>> response: `, response);
+
+	if (response.ok) {
+		const subs = await response.json();
+		dispatch(actionReadSubreadits(subs));
+		return subs;
+	}
+};
+
+// GET: Get Subreadit Details Route: /api/sub/:subId
+export const thunkReadSubreaditDetails = (payload) => async (dispatch) => {
+	const response = await csrfFetch(`/api/sub/${payload.subId}`);
+
+	// console.log(`thunk>>> response: `, response);
+	if (response.ok) {
+		const subs = await response.json();
+		dispatch(actionReadSubreaditDetails(subs));
+		return subs;
+	}
+};
+
+// POST: Create a Group Route: /api/sub
+export const thunkCreateSubreadit = (payload) => async (dispatch) => {
+	const response = await csrfFetch(`/api/sub`, {
+		method: `POST`,
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(payload),
+	});
+
+	if (response.ok) {
+		const subs = await response.json();
+		dispatch(actionCreateSubreadit(subs));
+		return subs;
+	}
+};
+
+// PUT: Edit a Subreadit Route: /api/sub/:subId
+export const thunkUpdateSubreadit = (payload) => async (dispatch) => {
+	const response = await csrfFetch(`/api/sub/${payload.id}`, {
+		method: `PUT`,
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(payload),
+	});
+
+	if (response.ok) {
+		const subs = await response.json();
+		dispatch(actionUpdateSubreadit(subs));
+		return subs;
+	}
+};
+
+// DELETE: Delete Subreadit Route: /api/sub/:subId
+export const thunkDeleteSubreadit = (payload) => async (dispatch) => {
+	const response = await csrfFetch(`/api/sub/${payload.subId}`, {
+		method: `DELETE`,
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(payload),
+	});
+
+	// console.log(`group id in THUNK>>>>>>>`,response);
+	if (response.ok) {
+		const subs = await response.json();
+		dispatch(actionDeleteSubreadit(subs));
+		return subs;
+	}
+};
+
+//----------------------------------------------
+
+//reducer
+
+function defaultState() {
+	const initialState = {};
+	return initialState;
+}
+
+const subreaditsReducer = (state = defaultState(), action) => {
+	switch (action.type) {
+		case READ_SUBREADITS: {
+			// console.log(`reducer>>> ACTION: `, action);
+
+			const newSubs = action.subreadits.reduce((acc, sub) => {
+				acc[sub.id] = sub;
+				return acc;
+			}, {});
+			return {
+				...state,
+				...newSubs,
+			};
+		}
+
+		case READ_SUBREADIT_DETAILS: {
+			// console.log(`reducer>>> ACTION: `, action.subreadit);
+			const newSub = { ...action.subreadit };
+			return {
+				...state,
+				[action.subreadit.id]: newSub,
+			};
+		}
+
+		case CREATE_SUBREADIT: {
+			const newState = {
+				...state,
+				[action.group.id]: action.group,
+			};
+			return newState;
+		}
+
+		case UPDATE_SUBREADIT: {
+			return { ...state, ...(state[action.group.id] = action.group) };
+		}
+
+		case DELETE_SUBREADIT: {
+			const newState = { ...state };
+			delete newState[action.group.id];
+			return newState;
+		}
+
+		case RESET_SUBREADIT:
+			return defaultState();
+
+		default:
+			return state;
+	}
+};
+
+export default subreaditsReducer;
