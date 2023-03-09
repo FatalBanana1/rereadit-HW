6cc42847da0b62864c7bb3f31414fb812b42ca7f
Henry W <henry.woodmansee@gmail.com>
2023-03-09 15:58:16 -0800

    added Subreadit.id to group clause on postsSubId scope

--- backend/db/models/post.js
+++ backend/db/models/post.js
@@ -121,7 +121,7 @@ module.exports = (sequelize, DataTypes) => {
 							}
 						],
 						order: [["id", "DESC"]],
-						group: ["Post.id", "User.id"]
+						group: ["Post.id", "User.id", "Subreadit.id"]
 					};
 				},
 				singlePost() {
